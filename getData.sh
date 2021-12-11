8b645d0438a591088f15b2b219c8f1fa23d1f096
Zgg2001 <397606244@qq.com>
2021-12-11 20:40:37 +0800

    获取室内外天气信息并打印

--- getData.sh
+++ getData.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #室外天气与气温
-cat /home/pi/work/outdoor/data
+cat /home/pi/Weather-Pi/outdoor/data
 #室内温湿度
-/usr/bin/python3 /home/pi/work/indoor/getIndoorT.py
+/usr/bin/python3 /home/pi/Weather-Pi/indoor/getIndoorT.py
 #ip获取 优先检测wlan0随后检测eth0
 wlan=`ifconfig wlan0 | grep inet | awk 'NR==1{print $2}'`
 eth=`ifconfig eth0 | grep inet | awk 'NR==1{print $2}'`
