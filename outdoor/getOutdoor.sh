8188a034584a9bf81d7320fc938053f09983e307
Zgg2001 <397606244@qq.com>
2021-12-11 20:39:46 +0800

    获取室外天气与气温的脚本

--- outdoor/getOutdoor.sh
+++ outdoor/getOutdoor.sh
@@ -2,6 +2,6 @@
 
 curl -sS wttr.in/ZhengZhou | awk 'NR>2&&NR<7{print}' > /home/pi/Weather-Pi/outdoor/weather
 weather=`cat /home/pi/Weather-Pi/outdoor/weather | awk 'NR==1{print}' | grep -o [A-Z].*[a-z]`
-temp=`cat /home/pi/Weather-Pi/outdoor/weather | awk 'NR==2{print}' | grep -o "\+.*°C"`
+temp=`cat /home/pi/Weather-Pi/outdoor/weather | awk 'NR==2{print}' | grep -o "\+[0-9]*"`
 echo "Weather: $weather" > /home/pi/Weather-Pi/outdoor/data
-echo "O TEMP: $temp" >> /home/pi/Weather-Pi/outdoor/data
+echo "O TEMP: $temp °C" >> /home/pi/Weather-Pi/outdoor/data
